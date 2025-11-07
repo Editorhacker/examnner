23863263b9ba38c6f26bc61bd1681a60e9109dff
Editorhacker <94226999+Editorhacker@users.noreply.github.com>
2025-11-07 19:11:24 +0530

    Delete examnner directory

--- examnner/routes/addPapers.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// Render the addPapers page
-router.get("/", (req, res) => {
-    res.render("ExamDepartment/addPapers");
-});
-
-module.exports = router;
